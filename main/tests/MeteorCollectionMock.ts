2edffa8fde882b65cdd9a4785e4f2f829323bb0a
menelike <human.ashrafi@gmail.com>
2020-09-25 10:04:01 +0200

    chore: replace mongoshell with custom node script to reduce dependencies

--- main/tests/MeteorCollectionMock.ts
+++ main/tests/MeteorCollectionMock.ts
@@ -1,8 +1,7 @@
 import { execSync } from 'child_process';
-import fs from 'fs';
+import path from 'path';
 
 import { Collection, FilterQuery } from 'mongodb';
-import tmp from 'tmp';
 
 import { MongoDoc } from '../src/types';
 
@@ -10,20 +9,7 @@ type FindOptions<T> = {
   fields?: { [P in keyof T]: boolean | number };
 };
 
-const mongoPath = process.env.MONGO_CLIENT_PATH || 'mongo';
-
-const mongoExists = (): void => {
-  try {
-    execSync(`${mongoPath} --version`, {
-      encoding: 'utf8',
-    });
-  } catch (err) {
-    console.debug(err);
-    throw Error(
-      `could not find the mongo binary (ENV.MONGO_CLIENT_PATH: ${mongoPath})`
-    );
-  }
-};
+const scriptPath = path.join(__dirname, 'MongoFind.js');
 
 export class MeteorCollection<T extends MongoDoc> {
   private readonly collection: Collection<T>;
@@ -33,7 +19,6 @@ export class MeteorCollection<T extends MongoDoc> {
   private readonly uri: string;
 
   constructor(uri: string, collection: Collection<T>, timeout: number) {
-    mongoExists();
     this.uri = uri;
     this.collection = collection;
     this.timeout = timeout;
@@ -48,54 +33,28 @@ export class MeteorCollection<T extends MongoDoc> {
     .mockImplementation((query?: FilterQuery<T>, options?: FindOptions<T>):
       | Array<Partial<T>>
       | undefined => {
-      let q = `db.${this.collection.collectionName}`;
+      let run = `node ${scriptPath} ${this.uri} ${this.collection.collectionName}`;
       if (query) {
-        if (options?.fields) {
-          q = `${q}.find(${JSON.stringify(query)}, ${JSON.stringify(
-            options?.fields
-          )})`;
-        } else {
-          q = `${q}.find(${JSON.stringify(query)})`;
-        }
-      } else if (options?.fields) {
-        q = `${q}.find(${JSON.stringify({})}, ${JSON.stringify(
-          options?.fields
-        )})`;
-      } else {
-        q = `${q}.find()`;
+        const buffer: Buffer = Buffer.from(JSON.stringify(query));
+        run = `${run} ${buffer.toString('base64')}`;
+      }
+      if (options?.fields) {
+        if (!query) run = `${run} undefined`;
+        const buffer: Buffer = Buffer.from(JSON.stringify(options.fields));
+        run = `${run} ${buffer.toString('base64')}`;
       }
 
-      q = `${q}.toArray()`;
-      q = `JSON.stringify(${q})`;
-
-      const tmpfile = tmp.fileSync();
-      fs.writeFileSync(tmpfile.name, q);
-
-      const run = `mongo --quiet --host ${this.uri} < ${tmpfile.name}`;
-
-      let result = '';
+      let result;
       try {
         result = execSync(run, {
           encoding: 'utf8',
         });
       } catch (err) {
-        console.debug(`QUERY: ${q}`);
+        console.debug(`FIND COMMAND: ${run}`);
         console.error(err);
         throw err;
       }
 
-      const lines = result.split('\n');
-
-      let res: Array<Partial<T>> | undefined;
-      lines.some((l) => {
-        try {
-          res = JSON.parse(l);
-          return true;
-        } catch (err) {
-          return false;
-        }
-      });
-
-      return res;
+      return JSON.parse(result);
     });
 }
